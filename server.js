b333e6a295802fbd1b998214d00c1f5badc6063e
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-10 16:53:06 +0100

    adding routes and server

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const morgan = require('morgan');
+const {getPosts} = require('./routes/post');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from express server'));
+const middleware = (req, res, next) => {
+    console.log('middleware applied');
+    next();
+}
+
+//middlewares
+app.use(morgan("dev"));
+app.use(middleware);
+
+//routes
+app.get('/', getPosts);
 
 const port = 5000;
 app.listen(port, () => console.log(`Server connected on port ${port}`));
