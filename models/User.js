6825034d8d883ffc288f196b091f5fd8eba9754d
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-10 20:33:01 +0100

    encrypting the user password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const uuid = require('uuid/v4');
+const crypto = require('crypto');
 
 const UserSchema = new mongoose.Schema = ({
     name: {
@@ -22,5 +24,34 @@ const UserSchema = new mongoose.Schema = ({
     },
     updated: Date
 });
+
+//virtual field
+userSchema.virtual('password')
+.set(function(password){
+    //create temp variable 
+    this._password = password
+    // generate a tiemstamp
+    this.salt = uuid()
+    //encrypt password
+    this.password = this.encryptPassword(password);
+})
+.get(function(){
+    return this._password
+})
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if(!password) return "";
+
+        try {
+          return crypto.createHmac('sha1', this.salt)
+            .update(password)
+            .digest('hex');
+        }
+        catch(err){
+           return ""
+        }
+    }
+}
 
 module.exports = mongoose.model("User", UserSchema);
