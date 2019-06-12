d91362650936f64cfa3aaa5c60e3e449b8b8c4e0
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-12 13:55:52 +0100

    update user and route

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const _ = require('lodash');
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
@@ -24,6 +25,29 @@ exports.allUsers = (req, res) => {
     if(err){
       res.status(400).json({error: err})
     }
-  })
-  res.json({users});
+    res.json({users});
+  }).select("name email updated created")
+}
+
+
+exports.getUser = (req, res) => {
+  req.profile.hash_password = undefined;
+  req.profile.salt = undefined;
+  return res.json(req.profile);
+}
+
+
+exports.updateUser = (req, res) => {
+    let user = req.profile;
+    user = _.extend(user, req.body) // mutate the source object ie user
+    user.updated = Date.now();
+
+    user.save(err => {
+      if(err){
+        return res.status(400).json({error: "You are not authorized to perform this action"})
+      }
+      user.hash_password = undefined;
+      user.salt = undefined;
+      res.json({user});
+    });
 }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const {userById, allUsers} = require('../controllers/user');
+const {userById, allUsers, getUser, updateUser} = require('../controllers/user');
+const {protectedRoute} = require('../controllers/auth');
 const router = express.Router();
 
 router.get('/user/all', allUsers);
+router.get('/user/:id', protectedRoute, getUser);
+router.put('/user/:id', protectedRoute, updateUser);
 
 //any route containing userid, will execute userbyID
-router.param("userId", userById);
+router.param("id", userById);
 
 module.exports = router;
 
