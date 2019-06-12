b04eb99384f57ea6e103f19b8e975acb5524d6c8
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-12 15:20:31 +0100

    updating our create posts and adding postsbyuser

--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,12 @@
 const Post = require('../models/Post');
+const formidable = require('formidable');
+const fs = require('fs');
 
 
 exports.getPosts = async(req, res) => {
     try{
-        const posts = await Post.find().select("_id title body");
+        const posts = await Post.find().populate("postedBy", "_id name")
+           .select("_id title body")
         res.status(200).json(posts);
     }
     catch(err){
@@ -11,14 +14,43 @@ exports.getPosts = async(req, res) => {
     }
 };
 
-exports.createPosts = async(req, res) => {
-    try{
-      const post = new Post(req.body);
-      await post.save();
-      res.status(200).json(post);
-    }
-    catch(err){
-     console.error(err.message);
-    }
+exports.createPosts = (req, res, next) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+
+        if(err){
+            return res.status(400).json({error: "Image could not be uploaded"})
+        }
+
+        let post = new Post(fields);
+        req.profile.hash_password = undefined;
+        req.profile.salt = undefined;
+        post.postedBy = req.profile;
+
+        if(files.photo){
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+
+        post.save((err, result) => {
+            if(err){
+                return res.status(400).json({error: err});
+            }
+            res.json(result);
+        });
+    });
+}
+
+exports.postsByUser = (req, res) => {
+    Post.find({postedBy: req.profile._id})
+          .populate("postedBy", "_id name")
+          .sort("_created")
+          .exec((err, posts) => {
+              if(err){
+                  return res.status(400).json({error: err});
+              }
+              res.json(posts);
+          })
 }
 
--- routes/post.js
+++ routes/post.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const {createPosts, getPosts} = require('../controllers/post');
+const {createPosts, getPosts, postsByUser} = require('../controllers/post');
 const {protectedRoute} = require('../controllers/auth');
 const {createPostValidator} = require('../validators');
 const {userById} = require('../controllers/user');
 const router = express.Router();
 
 router.get('/post/all', getPosts);
-router.post('/post', protectedRoute, createPostValidator, createPosts);
+router.post('/post/new/:id', protectedRoute, createPosts, createPostValidator);
+router.get("/post/by/:id", protectedRoute, postsByUser);
 
 //any route containing userid, will execute userbyID
-router.param("userId", userById);
+router.param("id", userById);
 
 module.exports = router;
 
