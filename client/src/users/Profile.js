8a63cdc1cf31e8bf2facc9de590c5de97f0cc7fc
rick427 <44168932+rick427@users.noreply.github.com>
2019-06-13 09:21:58 +0100

    if user is authenticated, add edit & delete btn

--- client/src/users/Profile.js
+++ client/src/users/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {isAuthenticated, profileRequest} from '../context';
-import {Redirect} from 'react-router-dom';
+import {Link, Redirect} from 'react-router-dom';
 
 class Profile extends Component {
     state = {
@@ -10,12 +10,14 @@ class Profile extends Component {
 
     componentDidMount(){
         const id = this.props.match.params.id;
-        this.initialize(id);   
+        this.initialize(id);
+        console.log("default props",this.props);
     }
 
     // initialize the id of the user to make a request to the backend api
     initialize = (id) => {
         const {token} = isAuthenticated();
+
         profileRequest(id, token)
         .then(data => {
             if(data.error) {
@@ -34,12 +36,28 @@ class Profile extends Component {
         if(redirect){
             return <Redirect to="/login"/>
         }
+        console.log("after setstate", this.state.user)
         return (
             <div className="container">
-                <h2 className="mt-5 mb-5">Profile</h2>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
-                <p>{`Joined ${new Date(this.state.user.created).toDateString()}`}</p>
+                <div className="row">
+                    <div className="col-md-6">
+                        <h2 className="mt-5 mb-5">Profile</h2>
+                        <p>{user.name}</p>
+                        <p>{user.email}</p>
+                        <p>{`Joined ${new Date(this.state.user.created).toDateString()}`}</p>
+                    </div>
+
+                    <div className="col-md-6">
+                        {user && user._id === this.state.user._id &&(
+                            <div className="d-inline-block mt-5">
+                                <Link to={`user/edit/${this.state.user._id}`} className="btn btn-raised btn-success mr-5">
+                                    Edit Profile
+                                </Link>
+                                <button className="btn btn-raised btn-danger">Delete Profile</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
             </div>
         )
     }
